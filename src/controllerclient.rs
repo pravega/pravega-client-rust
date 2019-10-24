3600ac39fe11840a43e4ba8c146c5b1088a4707c
Sandeep <sandeep.shridhar@emc.com>
2019-10-24 07:05:16 -0400

    CR Changes: Improve usage pattern.
    Signed-off-by: Sandeep <sandeep.shridhar@emc.com>

--- src/controllerclient.rs
+++ src/controllerclient.rs
@@ -25,21 +25,26 @@ fn create_connection(uri: &'static str) -> ControllerServiceClient<Channel> {
 
 /// Async function to create scope
 async fn create_scope(
-    request: tonic::Request<ScopeInfo>,
+    request: ScopeInfo,
     ch: &mut ControllerServiceClient<Channel>,
-) -> Result<tonic::Response<CreateScopeStatus>, tonic::Status> {
-    ch.create_scope(request).await
+) -> CreateScopeStatus {
+    let op_status: tonic::Response<CreateScopeStatus> = ch
+        .create_scope(tonic::Request::new(request))
+        .await
+        .expect("Failed to CreateScope");
+    op_status.into_inner() // return the scope status
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
     // start Pravega standalone before invoking this function.
     let mut client = create_connection("http://[::1]:9090");
-    let request = tonic::Request::new(ScopeInfo {
+    let request = ScopeInfo {
         scope: "testScope123".into(),
-    });
-    let response = create_scope(request, &mut client).await?;
+    };
+    let response: CreateScopeStatus = create_scope(request, &mut client).await;
     println!("Response for create_scope is {:?}", response);
+
     Ok(())
 }
 
@@ -50,22 +55,17 @@ mod tests {
     use tokio::runtime::Runtime;
 
     #[test]
+    #[should_panic] // since the controller is not running.
     fn test_create_scope_error() {
         let rt = Runtime::new().unwrap();
 
         let mut client = create_connection("http://[::1]:9090");
 
-        let request = tonic::Request::new(ScopeInfo {
+        let request = ScopeInfo {
             scope: "testScope124".into(),
-        });
+        };
         let fut = create_scope(request, &mut client);
 
-        let r: Result<tonic::Response<controller::CreateScopeStatus>, tonic::Status> =
-            rt.block_on(fut);
-
-        assert!(
-            r.is_err(),
-            "connection should fail since controller is not running"
-        );
+        rt.block_on(fut);
     }
 }
