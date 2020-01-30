4f9ac3c0a2bfa1a7ba298c46ebdd743a6705ba4f
zhangw27 <Wenxiao.Zhang@emc.com>
2020-01-30 12:25:03 -0800

    add doc test

--- retry/src/lib.rs
+++ retry/src/lib.rs
@@ -1,20 +1,49 @@
 //! Retry is a crate for retrying something that can fail with exponential backoff.
 //! It is designed to have a declarative interface for ease of use.
 //! It can be used as follows:
+//! ```
+//! # use pravega_rust_client_retry::retry_policy::RetryWithBackoff;
+//! # use pravega_rust_client_retry::retry_result::Retry;
+//! # use pravega_rust_client_retry::retry::retry;
+//! let retry_policy = RetryWithBackoff::default().max_tries(1);
+//! let mut collection = vec![1, 2].into_iter();
+//! let value = retry(retry_policy, || match collection.next() {
+//!     Some(n) if n == 2 => Ok(n),
+//!     Some(_) => Err(Retry::Retry("not 2")),
+//!     None => Err(Retry::Err("to the end")),
+//! }).unwrap();
 //!
+//! assert_eq!(value, 2);
 //!
-//!
-//!
-//!
-//! The above will retry the code in the block up to 5 times if it throws Error.
-//! If it throws a RuntimeException or returns successfully it will throw or return immediately.
-//! The delay following each of the filed attempts would be 1, 10, 100, 1000, and 10000ms respectively.
-//! If all retries fail {@link RetriesExhaustedException} will be thrown.
+//! ```
+//! The above will retry the code  1 times if it throws Err(Retry::Retry).
+//! If it throws a Err(Retry::Err) or returns successfully it will return immediately.
+//! The delay following each of the filed attempts would be 1, 10,respectively.
+//! If all retries fail, it will return Err(RetryErr) that has error message.
 //!
 
-mod retry;
-mod retry_asyn;
-mod retry_policy;
-mod retry_result;
+#![deny(
+    clippy::all,
+    clippy::cargo,
+    clippy::else_if_without_else,
+    clippy::empty_line_after_outer_attr,
+    clippy::multiple_inherent_impl,
+    clippy::mut_mut,
+    clippy::path_buf_push_overwrite
+)]
+#![warn(
+    clippy::cargo_common_metadata,
+    clippy::mutex_integer,
+    clippy::needless_borrow,
+    clippy::option_unwrap_used,
+    clippy::result_unwrap_used,
+    clippy::similar_names
+)]
+#![allow(clippy::multiple_crate_versions)]
+
+pub mod retry;
+pub mod retry_asyn;
+pub mod retry_policy;
+pub mod retry_result;
 #[cfg(test)]
 mod test;
