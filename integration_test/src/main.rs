191767e71aefcde7439fa2dbdbd87b69beba5fe3
Wenxiao Zhang <wenxiao.zhang@emc.com>
2020-02-13 13:56:33 -0800

    cargo fmt

--- integration_test/src/main.rs
+++ integration_test/src/main.rs
@@ -2,8 +2,4 @@ mod pravega_service;
 #[cfg(test)]
 mod tests;
 
-use pravega_service::PravegaService;
-use pravega_service::PravegaStandaloneService;
-use std::{thread, time};
-
 fn main() {}
--- integration_test/src/pravega_service.rs
+++ integration_test/src/pravega_service.rs
@@ -2,7 +2,7 @@ use log::info;
 use std::net::IpAddr;
 use std::net::Ipv4Addr;
 use std::net::SocketAddr;
-use std::process::{Child, Command, Stdio};
+use std::process::{Child, Command};
 
 const PATH: &'static str = "./pravega/bin/pravega-standalone";
 /**
