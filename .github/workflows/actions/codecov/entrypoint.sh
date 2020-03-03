4c8714cc769c860a286f683cabc3d97e360a1830
wenqimou <452787782@qq.com>
2020-03-03 12:14:53 -0800

    move to private
    Signed-off-by: wenqimou <452787782@qq.com>

---
+++ .github/workflows/actions/codecov/entrypoint.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+
+set -e
+
+DESTDIR="../../kcov-build"
+BUILD="build"
+KCOVMASTER="kcov-master"
+
+echo "Getting kcov files"
+sh -c "wget -nv https://github.com/SimonKagstrom/kcov/archive/master.tar.gz"
+
+echo "Extracting kcov content"
+tar -xzf master.tar.gz
+
+ls -l
+
+echo "Moving into the extracted directory"
+cd $KCOVMASTER
+
+echo "Creating the build folder"
+mkdir $BUILD
+
+echo "Moving into the build folder"
+cd $BUILD
+
+cmake ..
+
+make
+
+make install $DESTDIR
+
+cd ../..
+
+rm -rf $KCOVMASTER
+
+for file in target/debug/examplerust-*
+do
+  [ -x "${file}" ] || continue
+  mkdir -p "target/cov/$(basename $file)"
+  ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"
+done
+
+bash <(curl -s https://codecov.io/bash)
+
+echo "Uploaded code coverage"
