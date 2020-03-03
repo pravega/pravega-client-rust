3cfe32ec8c7e2832e6a77266a751a9db3cc785f1
wenqimou <452787782@qq.com>
2020-03-03 15:56:42 -0800

    debug
    Signed-off-by: wenqimou <452787782@qq.com>

--- .github/workflows/codecov/codecov.sh
+++ .github/workflows/codecov/codecov.sh
@@ -2,15 +2,15 @@
 set -e
 
 apt-get update && \
-    apt-get install -y \
-    curl \
-    libcurl4-openssl-dev \
-    libelf-dev \
-    libdw-dev \
-    cmake \
-    gcc \
-    binutils-dev \
-    libiberty-dev
+apt-get install -y \
+curl \
+libcurl4-openssl-dev \
+libelf-dev \
+libdw-dev \
+cmake \
+gcc \
+binutils-dev \
+libiberty-dev
 
 wget https://github.com/SimonKagstrom/kcov/archive/master.tar.gz &&
 tar xzf master.tar.gz &&
@@ -21,8 +21,8 @@ cmake .. &&
 make &&
 make install DESTDIR=../../kcov-build &&
 cd ../.. &&
+ls &&
 rm -rf kcov-master &&
 for file in target/debug/incremental/pravega_client_rust-*; do [ -x "${file}" ] || continue; mkdir -p "target/cov/$(basename $file)"; ./kcov-build/usr/local/bin/kcov --exclude-pattern=/.cargo,/usr/lib --verify "target/cov/$(basename $file)" "$file"; done &&
-bash <(curl -s https://codecov.io/bash) -t 6af47fa4-eed7-4fce-adbd-77291bc74cac
-
+bash <(curl -s https://codecov.io/bash) -t 6af47fa4-eed7-4fce-adbd-77291bc74cac &&
 echo "Uploaded code coverage"
