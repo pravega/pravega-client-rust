96c50127c7dbaeb54b3d45c37ea8bb37ae7c9442
Sandeep <sandeep.shridhar@emc.com>
2020-04-28 23:37:14 -0400

    Fix fmt.
    Signed-off-by: Sandeep <sandeep.shridhar@emc.com>

--- src/lib.rs
+++ src/lib.rs
@@ -36,8 +36,8 @@ pub mod error;
 pub mod event_stream_writer;
 pub mod raw_client;
 pub mod segment_reader;
-pub mod tablemap;
 mod stream;
+pub mod tablemap;
 
 pub static REQUEST_ID_GENERATOR: AtomicI64 = AtomicI64::new(0);
 
