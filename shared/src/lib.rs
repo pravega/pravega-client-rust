d364f346631c3cee67903bc13f97b45fdb3dbc39
wenqimou <452787782@qq.com>
2020-05-14 14:50:31 -0700

    minor fix
    Signed-off-by: wenqimou <452787782@qq.com>

--- shared/src/lib.rs
+++ shared/src/lib.rs
@@ -80,7 +80,10 @@ impl Segment {
     pub fn from_id_and_epoch(segment_id: i32, epoch: i32) -> Self {
         let epoch_i64 = (epoch as i64) << 32;
         let id_i64 = segment_id as i64;
-        Segment(epoch_i64 + id_i64)
+        Segment {
+            number: epoch_i64 + id_i64,
+            tx_id: None,
+        }
     }
 
     pub fn new_txn(number: i64, tx_id: TxId) -> Self {
