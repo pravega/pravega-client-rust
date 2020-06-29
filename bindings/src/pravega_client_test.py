a061030ed4deba7c641e1fe195a4cb5effb10116
Sandeep <sandeep.shridhar@emc.com>
2020-06-29 15:06:03 +0530

    enable tests on different python versions.
    Signed-off-by: Sandeep <sandeep.shridhar@emc.com>

--- bindings/src/pravega_client_test.py
+++ bindings/src/pravega_client_test.py
@@ -9,43 +9,49 @@
 #
 
 import unittest
+import secrets
+import string
 import pravega_client;
 from pravega_client import TxnFailedException;
 
 class PravegaTest(unittest.TestCase):
     def test_writeEvent(self):
+        scope = ''.join(secrets.choice(string.ascii_lowercase + string.digits)
+                      for i in range(10))
         print("Creating a Stream Manager, ensure Pravega is running")
         stream_manager=pravega_client.StreamManager("127.0.0.1:9090")
 
         print("Creating a scope")
-        scope_result=stream_manager.create_scope("testScope")
+        scope_result=stream_manager.create_scope(scope)
         self.assertEqual(True, scope_result, "Scope creation status")
 
         print("Creating a stream")
-        stream_result=stream_manager.create_stream("testScope", "testStream", 1)
+        stream_result=stream_manager.create_stream(scope, "testStream", 1)
         self.assertEqual(True, stream_result, "Stream creation status")
 
         print("Creating a writer for Stream")
-        w1=stream_manager.create_writer("testScope","testStream")
+        w1=stream_manager.create_writer(scope,"testStream")
 
         print("Write events")
         w1.write_event("test event1")
         w1.write_event("test event2")
 
     def test_writeTxn(self):
+        scope = ''.join(secrets.choice(string.ascii_lowercase + string.digits)
+                        for i in range(10))
         print("Creating a Stream Manager, ensure Pravega is running")
         stream_manager=pravega_client.StreamManager("127.0.0.1:9090")
 
         print("Creating a scope")
-        scope_result=stream_manager.create_scope("testScope1")
+        scope_result=stream_manager.create_scope(scope)
         self.assertEqual(True, scope_result, "Scope creation status")
 
         print("Creating a stream")
-        stream_result=stream_manager.create_stream("testScope1", "testTxn", 1)
+        stream_result=stream_manager.create_stream(scope, "testTxn", 1)
         self.assertEqual(True, stream_result, "Stream creation status")
 
         print("Creating a txn writer for Stream")
-        w1=stream_manager.create_transaction_writer("testScope1","testTxn", 1)
+        w1=stream_manager.create_transaction_writer(scope,"testTxn", 1)
         txn1 = w1.begin_txn()
         print("Write events")
         txn1.write_event("test event1")
@@ -65,19 +71,22 @@ class PravegaTest(unittest.TestCase):
         self.assertEqual(False, txn2.is_open(), "Transaction is closed")
 
     def test_TxnError(self):
+        scope = ''.join(secrets.choice(string.ascii_lowercase + string.digits)
+                        for i in range(10))
+
         print("Creating a Stream Manager, ensure Pravega is running")
         stream_manager=pravega_client.StreamManager("127.0.0.1:9090")
 
         print("Creating a scope")
-        scope_result=stream_manager.create_scope("testScope2")
+        scope_result=stream_manager.create_scope(scope)
         self.assertEqual(True, scope_result, "Scope creation status")
 
         print("Creating a stream")
-        stream_result=stream_manager.create_stream("testScope2", "testTxn", 1)
+        stream_result=stream_manager.create_stream(scope, "testTxn", 1)
         self.assertEqual(True, stream_result, "Stream creation status")
 
         print("Creating a txn writer for Stream")
-        w1=stream_manager.create_transaction_writer("testScope2","testTxn", 1)
+        w1=stream_manager.create_transaction_writer(scope,"testTxn", 1)
         txn1 = w1.begin_txn()
         print("Write events")
         txn1.write_event("test event1")
