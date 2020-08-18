4c3c938651d0c5a9ae9421ca6ce6efb8fd4fad2f
Sandeep <sandeep.shridhar@emc.com>
2020-08-18 16:38:11 +0530

    Improve test coverage, verify offset returned.
    Signed-off-by: Sandeep <sandeep.shridhar@emc.com>

--- src/segment_slice.rs
+++ src/segment_slice.rs
@@ -13,13 +13,16 @@ use crate::segment_reader::{AsyncSegmentReader, AsyncSegmentReaderImpl};
 use async_recursion::async_recursion;
 use bytes::{Buf, BufMut, BytesMut};
 use pravega_rust_client_shared::ScopedSegment;
-use pravega_wire_protocol::commands::{Command, EventCommand};
+use pravega_wire_protocol::commands::{Command, EventCommand, TYPE_PLUS_LENGTH_SIZE};
 use std::sync::Arc;
 use tokio::runtime::Handle;
 use tokio::sync::mpsc;
 use tokio::sync::mpsc::{Receiver, Sender};
-use tracing::{debug, info, warn};
+use tracing::{debug, error, info, warn};
 
+///
+///
+///
 #[derive(Debug)]
 pub struct Event {
     offset_in_segment: i64,
@@ -35,19 +38,17 @@ pub struct SegmentSlice {
     segment_data: BytePlaceholder,
     handle: Handle,
     partial_event: BytePlaceholder,
-}
-
-enum ReadType {
-    Event(Event),
-    PartialEvent(),
+    partial_header: BytePlaceholder,
 }
 
 ///
-/// Amount of Bytes Read ahead.
+/// Read Buffer size
 ///
 const READ_BUFFER_SIZE: i32 = 2048;
 
-// Structure to track the offset and byte array.
+///
+/// Structure to track the offset and byte array.
+///
 #[derive(Debug)]
 struct BytePlaceholder {
     offset_in_segment: i64,
@@ -55,17 +56,34 @@ struct BytePlaceholder {
 }
 
 impl BytePlaceholder {
+    ///
+    /// Removes the bytes from the current view, returning them in a new `BytePlaceholder` handle.
+    /// Afterwards, `self` will be empty.
+    /// This is identical to `self.split_to(length)`.
+    ///
     fn split(&mut self) -> BytePlaceholder {
         let res = self.value.split();
         let old_offset = self.offset_in_segment;
         let new_offset = old_offset + res.len() as i64;
         self.offset_in_segment = new_offset;
         BytePlaceholder {
-            offset_in_segment: self.offset_in_segment,
+            offset_in_segment: old_offset,
             value: res,
         }
     }
 
+    ///
+    /// Splits the buffer into two at the given index.
+    ///
+    /// Afterwards `self` contains elements `[at, len)`, and the returned `BytePlaceholder`
+    /// contains elements `[0, at)`.
+    ///
+    /// `self.offset_in_segment` is updated accordingly.
+    ///
+    /// # Panics
+    ///
+    /// Panics if `at > len`.
+    ///
     fn split_to(&mut self, at: usize) -> BytePlaceholder {
         let old_offset = self.offset_in_segment;
         let res = self.value.split_to(at);
@@ -77,12 +95,24 @@ impl BytePlaceholder {
         }
     }
 
+    ///
+    /// Gets a signed 32 bit integer from `self` in big-endian byte order.
+    ///
+    /// The offset in segment is advanced by 4.
+    ///
+    /// # Panics
+    ///
+    /// This function panics if there is not enough remaining data in `self`.
+    ///
     fn get_i32(&mut self) -> i32 {
         let result = self.value.get_i32();
         self.offset_in_segment += 4;
         result
     }
 
+    ///
+    /// Returns an empty BytePlaceholder. The offset is set as 0.
+    ///
     fn empty() -> BytePlaceholder {
         BytePlaceholder {
             offset_in_segment: 0,
@@ -92,6 +122,11 @@ impl BytePlaceholder {
 }
 
 impl SegmentSlice {
+    ///
+    /// Create a new SegmentSlice for a given start_offset, segment.
+    /// This spawns an asynchronous task to fetch data from the segment with length of  `READ_BUFFER_SIZE`.
+    /// The channel buffer size is 1 which ensure only one outstanding read request to Segment store.
+    ///
     pub(crate) fn new(
         segment: ScopedSegment,
         start_offset: i64,
@@ -116,11 +151,12 @@ impl SegmentSlice {
             segment_data: BytePlaceholder::empty(),
             handle,
             partial_event: BytePlaceholder::empty(),
+            partial_header: BytePlaceholder::empty(),
         }
     }
 
     ///
-    /// Method to fetch segment data in a loop
+    /// Method to fetch data from the Segment store from a given start offset.
     ///
     async fn get_segment_data(
         segment: ScopedSegment,
@@ -141,6 +177,7 @@ impl SegmentSlice {
                     debug!("Read Response from Segment store {:?}", reply);
                     let len = reply.data.len();
                     if len == 0 || reply.end_of_segment {
+                        drop(tx);
                         break;
                     } else {
                         let segment_data = bytes::BytesMut::from(reply.data.as_slice());
@@ -164,19 +201,43 @@ impl SegmentSlice {
         }
     }
 
+    ///
+    /// Return the starting offset of the SegmentSlice.
+    ///
     fn get_starting_offset(&self) -> i64 {
         self.start_offset
     }
 
+    ///
+    /// Return the segment associated with the SegmentSlice.
+    ///
     fn get_segment(&self) -> String {
         //Returns the name of the segment
         self.segment.to_string()
     }
 
     ///
-    /// Extract event from the data received from the Segment store. Return None in case of a Partial event.
+    /// Extract the next event from the data received from the Segment store.
+    /// Return None in case of a Partial data.
     ///
-    fn extract_event(&mut self, parse_header: fn(&mut BytePlaceholder) -> BytePlaceholder) -> Option<Event> {
+    fn extract_event(
+        &mut self,
+        parse_header: fn(&mut BytePlaceholder) -> Option<BytePlaceholder>,
+    ) -> Option<Event> {
+        // check if a partial header was read last.
+        if self.partial_header.value.capacity() != 0 {
+            let bytes_to_read = TYPE_PLUS_LENGTH_SIZE as usize - self.partial_header.value.capacity();
+            let t = self.segment_data.split_to(bytes_to_read);
+            self.partial_header.value.put(t.value);
+            if let Some(temp_event) = parse_header(&mut self.partial_header) {
+                self.partial_event = temp_event;
+                self.partial_header = BytePlaceholder::empty();
+            } else {
+                error!("Invalid Header length observed");
+                panic!("Invalid Header length");
+            }
+        }
+        // check if a partial event was read the last time.
         if self.partial_event.value.capacity() != 0 {
             let bytes_to_read = self.partial_event.value.capacity() - self.partial_event.value.len();
             if self.segment_data.value.remaining() >= bytes_to_read {
@@ -193,14 +254,14 @@ impl SegmentSlice {
                 self.partial_event.value.put(self.segment_data.value.split());
                 None
             }
-        } else {
-            let mut event_data = parse_header(&mut self.segment_data);
+        } else if let Some(mut event_data) = parse_header(&mut self.segment_data) {
             let bytes_to_read = event_data.value.capacity();
             if bytes_to_read == 0 {
                 warn!("Found a header with length as zero");
                 return None;
             }
             if self.segment_data.value.remaining() >= bytes_to_read {
+                // all the data of the event is already present.
                 let t = self.segment_data.split_to(bytes_to_read);
                 event_data.value.put(t.value);
                 info!(
@@ -215,6 +276,7 @@ impl SegmentSlice {
                 };
                 Some(event)
             } else {
+                // complete data for a given event is not present in the buffer.
                 let t = self.segment_data.split();
                 event_data.value.put(t.value);
                 debug!(
@@ -226,45 +288,40 @@ impl SegmentSlice {
                 self.partial_event = event_data;
                 None
             }
+        } else {
+            // partial header was read
+            self.partial_header = self.segment_data.split();
+            None
         }
     }
 
     ///
     /// This method reads the header and returns a BytesMut whose size is as big as the event.
+    /// If complete header is not present return None.
     ///
-    fn read_header(mut data: &mut BytePlaceholder) -> BytePlaceholder {
-        let event_offset = data.offset_in_segment;
-        let type_code = data.get_i32();
-        assert_eq!(type_code, EventCommand::TYPE_CODE);
-        let len = data.get_i32();
-        data.offset_in_segment += 8; //increase the offset in segment by
-        debug!("Event size is {}", len);
-        BytePlaceholder {
-            offset_in_segment: event_offset,
-            value: BytesMut::with_capacity(len as usize),
-        }
-    }
-
-    async fn read_next_event(
-        &mut self,
-        parse_header: fn(&mut BytePlaceholder) -> BytePlaceholder,
-    ) -> Option<Event> {
-        if self.segment_data.value.is_empty() {
-            if let Some(response) = self.event_rx.recv().await {
-                self.segment_data = response;
-                self.extract_event(parse_header)
-            } else {
-                None
-            }
+    fn read_header(data: &mut BytePlaceholder) -> Option<BytePlaceholder> {
+        if data.value.len() >= TYPE_PLUS_LENGTH_SIZE as usize {
+            let event_offset = data.offset_in_segment;
+            let type_code = data.get_i32();
+            assert_eq!(type_code, EventCommand::TYPE_CODE, "Expected EventCommand here.");
+            let len = data.get_i32();
+            debug!("Event size is {}", len);
+            Some(BytePlaceholder {
+                offset_in_segment: event_offset,
+                value: BytesMut::with_capacity(len as usize),
+            })
         } else {
-            self.extract_event(parse_header)
+            None
         }
     }
 
+    ///
+    /// This method attempts fetch the data from the
+    ///
     #[async_recursion]
     async fn fetch_next_event(
         &mut self,
-        parse_header: fn(&mut BytePlaceholder) -> BytePlaceholder,
+        parse_header: fn(&mut BytePlaceholder) -> Option<BytePlaceholder>,
     ) -> Option<Event> {
         if self.segment_data.value.is_empty() {
             if let Some(response) = self.event_rx.recv().await {
@@ -293,7 +350,6 @@ impl Iterator for SegmentSlice {
     fn next(&mut self) -> Option<Self::Item> {
         // Wait for a response from the segment store only if we do not have pre-fetched data or in case
         // of a partial event.
-
         if self.segment_data.value.is_empty() {
             if let Some(response) = self.handle.block_on(self.event_rx.recv()) {
                 self.segment_data = response;
@@ -302,8 +358,12 @@ impl Iterator for SegmentSlice {
             }
         }
         let res = self.extract_event(SegmentSlice::read_header);
+
         match res {
-            Some(d) => Some(d),
+            Some(event) => {
+                self.read_offset = event.offset_in_segment;
+                Some(event)
+            }
             None => {
                 info!("Partial event read by the extract_event method, invoke read again to fetch the complete event.");
                 self.next()
@@ -324,39 +384,23 @@ mod tests {
         StreamConfiguration,
     };
     use pravega_wire_protocol::client_config::{ClientConfigBuilder, TEST_CONTROLLER_URI};
+    use std::iter;
     use tokio::sync::mpsc;
     use tokio::sync::mpsc::{Receiver, Sender};
 
     ///
     /// This method reads the header and returns a BytesMut whose size is as big as the event.
     ///
-    fn custom_read_header(data: &mut BytePlaceholder) -> BytePlaceholder {
+    fn custom_read_header(data: &mut BytePlaceholder) -> Option<BytePlaceholder> {
         if data.value.remaining() >= 4 {
             let len = data.get_i32();
             println!("Header length read is {}", len);
-            BytePlaceholder {
+            Some(BytePlaceholder {
                 offset_in_segment: 0,
                 value: BytesMut::with_capacity(len as usize),
-            }
+            })
         } else {
-            BytePlaceholder {
-                offset_in_segment: 0,
-                value: BytesMut::with_capacity(0),
-            }
-        }
-    }
-
-    #[tokio::test]
-    async fn test_read_partial_events() {
-        let (tx, rx) = mpsc::channel(1);
-        tokio::spawn(generate_multiple_constant_size_events(tx));
-        let mut segment_slice = create_segment_slice(rx);
-
-        let mut event_size = 0;
-        while let Some(event) = segment_slice.fetch_next_event(custom_read_header).await {
-            assert!(is_all_same12(event.value.as_slice()));
-            assert_eq!(event_size + 1, event.value.len(), "Event was missed out");
-            event_size = event_size + 1;
+            None
         }
     }
 
@@ -367,11 +411,57 @@ mod tests {
         let mut segment_slice = create_segment_slice(rx);
         let mut event_size = 0;
         while let Some(event) = segment_slice.fetch_next_event(custom_read_header).await {
-            assert!(is_all_same12(event.value.as_slice()));
+            assert!(is_all_same(event.value.as_slice()));
             assert_eq!(event_size + 1, event.value.len(), "Event was missed out");
             event_size = event_size + 1;
         }
         assert_eq!(event_size, 10);
+    }
+
+    #[tokio::test]
+    async fn test_read_partial_events_custom() {
+        let (tx, rx) = mpsc::channel(1);
+        tokio::spawn(generate_multiple_constant_size_events(tx));
+        let mut segment_slice = create_segment_slice(rx);
+
+        let mut event_size = 0;
+        while let Some(event) = segment_slice.fetch_next_event(custom_read_header).await {
+            assert!(is_all_same(event.value.as_slice()));
+            assert_eq!(event_size + 1, event.value.len(), "Event was missed out");
+            event_size = event_size + 1;
+        }
+    }
+
+    #[tokio::test]
+    async fn test_read_partial_events_buffer_10() {
+        let (tx, rx) = mpsc::channel(1);
+        tokio::spawn(generate_variable_size_events(tx, 10, 20));
+        let mut segment_slice = create_segment_slice(rx);
+        let mut expected_offset: usize = 0;
+        let mut expected_event_len = 1;
+        while let Some(d) = segment_slice.fetch_next_event(SegmentSlice::read_header).await {
+            assert_eq!(expected_offset, d.offset_in_segment as usize);
+            assert_eq!(expected_event_len, d.value.len());
+            assert!(is_all_same(d.value.as_slice()));
+            expected_offset += 8 + expected_event_len;
+            expected_event_len += 1;
+        }
+    }
+
+    #[tokio::test]
+    async fn test_read_partial_events_buffer_100() {
+        let (tx, rx) = mpsc::channel(1);
+        tokio::spawn(generate_variable_size_events(tx, 100, 200));
+        let mut segment_slice = create_segment_slice(rx);
+        let mut expected_offset: usize = 0;
+        let mut expected_event_len = 1;
+        while let Some(d) = segment_slice.fetch_next_event(SegmentSlice::read_header).await {
+            assert_eq!(expected_offset, d.offset_in_segment as usize);
+            assert_eq!(expected_event_len, d.value.len());
+            assert!(is_all_same(d.value.as_slice()));
+            expected_offset += 8 + expected_event_len;
+            expected_event_len += 1;
+        }
     }
 
     fn create_segment_slice(rx: Receiver<BytePlaceholder>) -> SegmentSlice {
@@ -393,17 +483,76 @@ mod tests {
             segment_data: BytePlaceholder::empty(),
             handle: Handle::current(),
             partial_event: BytePlaceholder::empty(),
+            partial_header: BytePlaceholder::empty(),
         };
         segment_slice
     }
 
-    fn is_all_same12<T: Eq>(slice: &[T]) -> bool {
+    ///
+    /// Helper method to verify all events read by Segment slice.
+    ///
+    fn is_all_same<T: Eq>(slice: &[T]) -> bool {
         slice
             .get(0)
             .map(|first| slice.iter().all(|x| x == first))
             .unwrap_or(true)
     }
 
+    // Generate events to simulate Pravega SegmentReadCommand.
+    async fn generate_variable_size_events(
+        mut tx: Sender<BytePlaceholder>,
+        buf_size: usize,
+        num_events: usize,
+    ) {
+        let mut buf = BytesMut::with_capacity(buf_size);
+        let mut offset: i64 = 0;
+        for i in 1..num_events {
+            let mut data = event_data(i);
+            if data.len() < buf.capacity() - buf.len() {
+                buf.put(data);
+            } else {
+                while data.len() > 0 {
+                    let free_space = buf.capacity() - buf.len();
+                    if free_space == 0 {
+                        tx.send(BytePlaceholder {
+                            offset_in_segment: offset,
+                            value: buf,
+                        })
+                        .await
+                        .unwrap();
+                        offset += buf_size as i64;
+                        buf = BytesMut::with_capacity(buf_size);
+                    } else if free_space >= data.len() {
+                        buf.put(data.split());
+                    } else {
+                        buf.put(data.split_to(free_space));
+                    }
+                }
+            }
+        }
+        // send the last event.
+        tx.send(BytePlaceholder {
+            offset_in_segment: offset,
+            value: buf,
+        })
+        .await
+        .unwrap();
+    }
+
+    // Generate event data given the lenth of the event.
+    // The data is 'a'
+    fn event_data(len: usize) -> BytesMut {
+        let mut buf = BytesMut::with_capacity(len + 8);
+        buf.put_i32(EventCommand::TYPE_CODE);
+        buf.put_i32(len as i32); // header
+
+        let mut data = Vec::new();
+        data.extend(iter::repeat(b'a').take(len));
+        buf.put(data.as_slice());
+        buf
+    }
+
+    // Custom multiple size events.
     async fn generate_multiple_constant_size_events(mut tx: Sender<BytePlaceholder>) {
         let mut buf = BytesMut::with_capacity(10);
 
@@ -506,7 +655,7 @@ mod tests {
                 })
                 .await
             {
-                println!("==> receiver dropped");
+                warn!("receiver dropped");
                 return;
             }
         }
@@ -546,12 +695,9 @@ mod tests {
         client_factory
             .get_runtime_handle()
             .block_on(slice.event_rx.recv());
-
-        // let s = client_factory
-        //     .get_runtime_handle()
-        //     .block_on(slice.event_rx.recv());
     }
 
+    // helper method to write events to pravega
     fn write_event(scope_name: Scope, stream_name: Stream, client_factory: &ClientFactory) {
         let controller_client = client_factory.get_controller_client();
         let handle = client_factory.get_runtime_handle();
@@ -574,6 +720,7 @@ mod tests {
         }
     }
 
+    // helper method to create scope and stream.
     async fn create_scope_stream(
         controller_client: &dyn ControllerClient,
         scope_name: &Scope,
