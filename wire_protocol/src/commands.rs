d30a06ad16166e9af62a02e37017e5a581355e37
wenqi <452787782@qq.com>
2021-01-06 19:09:37 -0800

    improve perf
    Signed-off-by: wenqi <452787782@qq.com>

--- wire_protocol/src/commands.rs
+++ wire_protocol/src/commands.rs
@@ -493,10 +493,9 @@ impl Command for EventCommand {
     fn write_fields(&self) -> Result<Vec<u8>, CommandError> {
         let mut res = Vec::with_capacity(self.data.len() + 4);
         res.extend_from_slice(&EventCommand::TYPE_CODE.to_be_bytes());
-        let encoded = CONFIG.serialize(&self).context(InvalidData {
+        CONFIG.serialize_into(&mut res, &self).context(InvalidData {
             command_type: Self::TYPE_CODE,
         })?;
-        res.extend(encoded);
         Ok(res)
     }
 
