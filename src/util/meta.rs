1a37cf1f22cda818f913643b0cf58902284ade0a
Sandeep <sandeep.shridhar@emc.com>
2021-10-06 12:40:10 +0530

    remove redundant code.
    Signed-off-by: Sandeep <sandeep.shridhar@emc.com>

--- src/util/meta.rs
+++ src/util/meta.rs
@@ -9,16 +9,10 @@
 //
 
 use crate::client_factory::ClientFactoryAsync;
-use crate::segment::raw_client::{RawClient, RawClientError};
-use crate::util::get_request_id;
 use im::HashMap;
 use pravega_client_auth::DelegationTokenProvider;
-use pravega_client_retry::retry_async::retry_async;
-use pravega_client_retry::retry_result::{RetryResult, Retryable};
-use pravega_client_shared::{ScopedSegment, ScopedStream, Segment};
+use pravega_client_shared::{ScopedSegment, ScopedStream, Segment, SegmentInfo};
 use pravega_controller_client::ControllerError;
-use pravega_wire_protocol::commands::{GetStreamSegmentInfoCommand, StreamSegmentInfoCommand};
-use pravega_wire_protocol::wire_commands::{Replies, Requests};
 use snafu::Snafu;
 use tracing::{debug, error};
 
@@ -48,11 +42,10 @@ pub enum MetaClientError {
         operation: String,
         error_msg: String,
     },
-    #[snafu(display("Could not connect due to {}", error_msg))]
-    ConnectionError {
+    #[snafu(display("Could not segment info after configured retries to {}", error_msg))]
+    SegmentMetaError {
         segment: String,
         can_retry: bool,
-        source: RawClientError,
         error_msg: String,
     },
     #[snafu(display("Could not connect due to {}", error_msg))]
@@ -62,82 +55,6 @@ pub enum MetaClientError {
         source: ControllerError,
         error_msg: String,
     },
-    #[snafu(display("Reader failed to perform reads {} due to {}", operation, error_msg,))]
-    AuthTokenCheckFailed {
-        segment: String,
-        can_retry: bool,
-        operation: String,
-        error_msg: String,
-    },
-    #[snafu(display("Could not connect due to {}", error_msg))]
-    AuthTokenExpired {
-        segment: String,
-        can_retry: bool,
-        source: RawClientError,
-        error_msg: String,
-    },
-    #[snafu(display("Could not connect due to {}", error_msg))]
-    WrongHost {
-        segment: String,
-        can_retry: bool,
-        operation: String,
-        error_msg: String,
-    },
-}
-impl MetaClientError {
-    fn refresh_token(&self) -> bool {
-        matches!(self, MetaClientError::AuthTokenExpired { .. })
-    }
-}
-
-impl Retryable for MetaClientError {
-    fn can_retry(&self) -> bool {
-        use MetaClientError::*;
-        match self {
-            StreamSealed {
-                stream: _,
-                can_retry,
-                operation: _,
-                error_msg: _,
-            } => *can_retry,
-            OperationError {
-                segment: _,
-                can_retry,
-                operation: _,
-                error_msg: _,
-            } => *can_retry,
-            ConnectionError {
-                segment: _,
-                can_retry,
-                source: _,
-                error_msg: _,
-            } => *can_retry,
-            AuthTokenCheckFailed {
-                segment: _,
-                can_retry,
-                operation: _,
-                error_msg: _,
-            } => *can_retry,
-            AuthTokenExpired {
-                segment: _,
-                can_retry,
-                source: _,
-                error_msg: _,
-            } => *can_retry,
-            WrongHost {
-                segment: _,
-                can_retry,
-                operation: _,
-                error_msg: _,
-            } => *can_retry,
-            ControllerConnectionError {
-                stream: _,
-                can_retry,
-                source: _,
-                error_msg: _,
-            } => *can_retry,
-        }
-    }
 }
 
 impl MetaClient {
@@ -184,8 +101,9 @@ impl MetaClient {
                     for (_, segment_range) in key_map {
                         let info = self.fetch_segment_info(&segment_range.scoped_segment).await;
                         match info {
-                            Ok(cmd) => {
-                                map.insert(segment_range.get_segment(), cmd.write_offset);
+                            Ok(segment_info) => {
+                                debug!("Received SegmentInfo {:?}", segment_info);
+                                map.insert(segment_range.get_segment(), segment_info.write_offset);
                             }
                             Err(e) => {
                                 error!(
@@ -227,89 +145,22 @@ impl MetaClient {
     async fn fetch_segment_info(
         &self,
         scoped_segment: &ScopedSegment,
-    ) -> Result<StreamSegmentInfoCommand, MetaClientError> {
-        retry_async(self.factory.config().retry_policy, || async {
-            let raw_client = self.factory.create_raw_client(scoped_segment).await;
-
-            let result = self.fetch_segment_info_inner(scoped_segment, &raw_client).await;
-            match result {
-                Ok(cmd) => RetryResult::Success(cmd),
-                Err(e) => {
-                    if e.can_retry() {
-                        if e.refresh_token() {
-                            self.delegation_token_provider.signal_token_expiry();
-                        }
-                        debug!(
-                            "Retry sending GetStreamSegmentInfo for segment {:?} due to {:?}",
-                            scoped_segment, e
-                        );
-                        RetryResult::Retry(e)
-                    } else {
-                        RetryResult::Fail(e)
-                    }
-                }
+    ) -> Result<SegmentInfo, MetaClientError> {
+        // Create a segment meta client for the specified segment.
+        let segment_meta_client = self
+            .factory
+            .create_segment_metadata_client(scoped_segment.clone())
+            .await;
+        segment_meta_client.get_segment_info().await.map_err(|e| {
+            error!(
+                "Failed to fetch Segment info for segment {:?}. Error {:?}",
+                scoped_segment, e
+            );
+            MetaClientError::SegmentMetaError {
+                segment: scoped_segment.to_string(),
+                can_retry: false,
+                error_msg: "Failed to fetch segment info for segment".to_string(),
             }
         })
-        .await
-        .map_err(|e| e.error)
-    }
-
-    // Method to initiate a GetStreamSegmentInfo wirecommand to the segment store.
-    async fn fetch_segment_info_inner(
-        &self,
-        scoped_segment: &ScopedSegment,
-        raw_client: &dyn RawClient<'_>,
-    ) -> Result<StreamSegmentInfoCommand, MetaClientError> {
-        let request = Requests::GetStreamSegmentInfo(GetStreamSegmentInfoCommand {
-            delegation_token: self
-                .delegation_token_provider
-                .retrieve_token(self.factory.controller_client())
-                .await,
-            request_id: get_request_id(),
-            segment_name: scoped_segment.to_string(),
-        });
-
-        let reply = raw_client.send_request(&request).await;
-        match reply {
-            Ok(reply) => match reply {
-                Replies::StreamSegmentInfo(cmd) => Ok(cmd),
-                Replies::AuthTokenCheckFailed(_cmd) => Err(MetaClientError::AuthTokenCheckFailed {
-                    segment: scoped_segment.to_string(),
-                    can_retry: false,
-                    operation: "Get Stream Segment Info".to_string(),
-                    error_msg: "Auth token expired".to_string(),
-                }),
-                Replies::WrongHost(_cmd) => Err(MetaClientError::WrongHost {
-                    segment: scoped_segment.to_string(),
-                    can_retry: true,
-                    operation: "Get Stream Segment Info".to_string(),
-                    error_msg: "Wrong host".to_string(),
-                }),
-
-                _ => {
-                    error!("Observed unexpected reply for GetStreamSegmentInfo {:?}", reply);
-                    Err(MetaClientError::OperationError {
-                        segment: scoped_segment.to_string(),
-                        can_retry: false,
-                        operation: "Get Stream Segment Info".to_string(),
-                        error_msg: "".to_string(),
-                    })
-                }
-            },
-            Err(error) => match error {
-                RawClientError::AuthTokenExpired { .. } => Err(MetaClientError::AuthTokenExpired {
-                    segment: scoped_segment.to_string(),
-                    can_retry: true,
-                    source: error,
-                    error_msg: "Auth token expired".to_string(),
-                }),
-                _ => Err(MetaClientError::ConnectionError {
-                    segment: scoped_segment.to_string(),
-                    can_retry: true,
-                    source: error,
-                    error_msg: "RawClient error".to_string(),
-                }),
-            },
-        }
     }
 }
