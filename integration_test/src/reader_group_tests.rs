6e0c0e3fec56ae4b6a3d3ea9214ff5ad4d3efa68
Sandeep <sandeep.shridhar@emc.com>
2021-11-26 16:57:19 +0530

    Improve test coverage.
    Signed-off-by: Sandeep <sandeep.shridhar@emc.com>

--- integration_test/src/reader_group_tests.rs
+++ integration_test/src/reader_group_tests.rs
@@ -9,6 +9,7 @@
 //
 
 use crate::pravega_service::PravegaStandaloneServiceConfig;
+use std::collections::HashMap;
 
 use pravega_client::client_factory::{ClientFactory, ClientFactoryAsync};
 use pravega_client::event::reader_group::{ReaderGroup, ReaderGroupConfigBuilder};
@@ -37,7 +38,7 @@ pub fn test_reader_group(config: PravegaStandaloneServiceConfig) {
         .expect("creating config");
     let client_factory = ClientFactory::new(config);
     let async_client_factory = client_factory.to_async();
-    test_read_offline_stream(&async_client_factory);
+    test_read_offline_with_offset(&async_client_factory);
 
     info!("test reader group finished");
 }
@@ -109,6 +110,87 @@ fn test_read_offline_stream(client_factory: &ClientFactoryAsync) {
 
     // Attempt reading the remaining events from the same reader marked offline.
     assert!(h.block_on(reader1.acquire_segment()).is_err());
+}
+
+fn test_read_offline_with_offset(client_factory: &ClientFactoryAsync) {
+    let h = client_factory.runtime_handle();
+    let scope_name = Scope::from("testReaderOfflineWithOffset".to_owned());
+    let stream_name = Stream::from("test".to_owned());
+    let str = ScopedStream {
+        scope: scope_name.clone(),
+        stream: stream_name.clone(),
+    };
+    const NUM_EVENTS: usize = 10;
+    const EVENT_SIZE: usize = 10;
+    // create scope and stream.
+    h.block_on(async {
+        let new_stream =
+            create_scope_stream(client_factory.controller_client(), &scope_name, &stream_name, 1).await;
+        new_stream
+    });
+
+    let rg_config = ReaderGroupConfigBuilder::default()
+        .read_from_head_of_stream(str)
+        .build();
+
+    let rg = h.block_on(client_factory.create_reader_group_with_config(
+        scope_name.clone(),
+        "rg_offline".to_string(),
+        rg_config,
+    ));
+    let reader_id = "r1";
+    let mut reader1 = h.block_on(rg.create_reader(reader_id.to_string()));
+
+    assert!(
+        h.block_on(reader1.acquire_segment()).unwrap().is_none(),
+        "No events are expected to be read"
+    );
+
+    // Write events to the stream.
+    h.block_on(write_events(
+        scope_name,
+        stream_name,
+        client_factory.clone(),
+        NUM_EVENTS,
+        EVENT_SIZE,
+    ));
+    // Verify that we are able to read events from the stream.
+    let mut slice = h
+        .block_on(reader1.acquire_segment())
+        .expect("Failed to acquire a segment")
+        .unwrap();
+    let event = slice.next().unwrap();
+    assert_eq!(
+        vec![1; EVENT_SIZE],
+        event.value.as_slice(),
+        "Corrupted event read"
+    );
+    let offset_map = HashMap::from([(
+        slice.meta.scoped_segment.clone(),
+        event.offset_in_segment + EVENT_SIZE as i64 + 8,
+    )]);
+
+    // Segment slice is dropped here and it will update the RG state with the offsets.
+    // Now mark the reader offline
+    let offline_res = h.block_on(rg.reader_offline(reader_id.to_string(), Some(offset_map)));
+    assert!(offline_res.is_ok(), "Reader offline did not succeed");
+    //create a new reader and attempt reading. We should read NUM_EVENTS-1 events.
+
+    let mut reader2 = h.block_on(rg.create_reader("r2".to_string()));
+    let slice = h
+        .block_on(reader2.acquire_segment())
+        .expect("Failed to acquire a segment")
+        .unwrap();
+    let mut event_count = 0;
+    for event in slice {
+        event_count += 1;
+        assert_eq!(
+            vec![1; EVENT_SIZE],
+            event.value.as_slice(),
+            "Corrupted event read"
+        );
+    }
+    assert_eq!(event_count, NUM_EVENTS - 1);
 }
 
 // helper method to write events to Pravega
