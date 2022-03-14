66ede75354025adcb1f43bf87eedff348ccb7ed8
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2022-03-14 16:07:01 +0800

    add header and arc
    Signed-off-by: thekingofcity <3353040+thekingofcity@users.noreply.github.com>

--- nodejs/src/stream_reader.rs
+++ nodejs/src/stream_reader.rs
@@ -146,7 +146,7 @@ impl StreamReader {
         let (deferred, promise) = cx.promise();
 
         // Spawn an `async` task on the tokio runtime.
-        let reader = stream_reader.reader.clone();
+        let reader = Arc::clone(&stream_reader.reader);
         stream_reader.runtime_handle.spawn(async move {
             // expensive async procrdure executed in the tokio thread
             let slice_result = reader.lock().await.acquire_segment().await;
--- nodejs/tests/stream_reader.ts
+++ nodejs/tests/stream_reader.ts
@@ -1,3 +1,16 @@
+// Copyright Pravega Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 import { StreamManager } from '../stream_manager.js';
 import { StreamCut } from '../stream_reader_group.js';
 
@@ -8,10 +21,10 @@ const stream_reader = stream_reader_group.create_reader('r1');
 
 try {
     const seg_slice = await stream_reader.get_segment_slice();
-    const enc = new TextDecoder('utf-8');
+    const dec = new TextDecoder('utf-8');
     for (const event of seg_slice) {
         const raw_value = event.data();
-        console.log(`Event at ${event.offset()} reads ${enc.decode(raw_value)}`);
+        console.log(`Event at ${event.offset()} reads ${dec.decode(raw_value)}`);
     }
 } catch (e) {
     console.log(e);
