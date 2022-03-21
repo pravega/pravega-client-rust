504933ef4e61f8aeee9716950c4b265d3fedbc59
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2022-03-21 15:01:20 +0800

    pretty docs
    Signed-off-by: thekingofcity <3353040+thekingofcity@users.noreply.github.com>

--- nodejs/README.md
+++ nodejs/README.md
@@ -1,35 +1,146 @@
-# Pravega Nodejs client.
+# Pravega Nodejs Client
 
-This project provides a way to interact with [Pravega](http://pravega.io) with a Nodejs client.
+This project provides a way to interact with [Pravega](https://cncf.pravega.io) with a Nodejs client.
 
 Pravega is an open source distributed storage service implementing Streams. It offers Stream as the main primitive for the foundation of reliable storage systems: a high-performance, durable, elastic, and unlimited append-only byte stream with strict ordering and consistency.
 
-Example usage can be found in `./tests` and you may run it with `node --loader ts-node/esm tests/stream_manager.ts`.
+## Install
+
+The client library can be installed using npm or yarn.
+
+```shell
+npm install @thekingofcity/pravega
+#or
+yarn add @thekingofcity/pravega
+```
+
+After the package is downloaded from the registry, a `node-pre-gyp install` will be triggered to pull the underlying Rust Node addon binary from the Github releases.
+
+Note your os and architecture matters. Only `Windows`, `MacOS`, and `linux` with `x86_64` comes with pre-built binary. If this fails or your platform is not supported, you need to build the native Node addon by pulling the repo and execute several commands to get the binary. They are stated below.
+
+## Example
+
+After a `npm init`, add `"type": "module",` to your `package.json` so `node` can load ECMAScript modules correctly.
+
+```javascript
+import { StreamCut, StreamManager } from '@thekingofcity/pravega';
+
+const SCOPE = 'scope1';
+const STREAM = 'stream1';
+const DATA = 'Hello World!';
+
+// Assume Pravega controller is listening at 127.0.0.1:9090
+const stream_manager = StreamManager('tcp://127.0.0.1:9090', false, false, true);
+// (Re)create a scope and stream.
+if (!stream_manager.list_scopes().includes(SCOPE)) {
+    stream_manager.create_scope(SCOPE);
+}
+if (stream_manager.list_streams(SCOPE).includes(STREAM)) {
+    stream_manager.seal_stream(SCOPE, STREAM);
+    stream_manager.delete_stream(SCOPE, STREAM);
+}
+// This will create a stream with only 1 segment.
+stream_manager.create_stream(SCOPE, STREAM);
+
+// Write event as string.
+const stream_writer_1 = stream_manager.create_writer(SCOPE, STREAM);
+await stream_writer_1.write_event(DATA);
+await stream_writer_1.write_event(DATA, 'routing_key');
+// Write event as bytes.
+const enc = new TextEncoder();
+const stream_writer_2 = stream_manager.create_writer(SCOPE, STREAM, 2);
+await stream_writer_2.write_event_bytes(enc.encode(DATA));
+await stream_writer_2.write_event_bytes(enc.encode(DATA), 'routing_key');
+// Since there can be max 2 inflight events not persisted, call `flush`.
+await stream_writer_2.flush();
+
+// Create a reader group and a reader.
+const reader_group_name = Math.random().toString(36).slice(2, 10);
+const reader_name = Math.random().toString(36).slice(2, 10);
+const stream_reader_group = stream_manager.create_reader_group(
+    StreamCut.head(),
+    reader_group_name,
+    SCOPE,
+    STREAM
+);
+const stream_reader = stream_reader_group.create_reader(reader_name);
+
+try {
+    // One `get_segment_slice()` call per segment.
+    const seg_slice = await stream_reader.get_segment_slice();
+    const dec = new TextDecoder('utf-8');
+    for (const event of seg_slice) {
+        const raw_bytes = event.data();
+        console.log(`Event at ${event.offset()} reads ${dec.decode(raw_bytes)}`);
+    }
+    // Release the current slice so other reader can lock and read this slice.
+    stream_reader.release_segment(seg_slice);
+} catch (e) {
+    console.log(e);
+} finally {
+    stream_reader_group.reader_offline(reader_name);
+}
+
+// Clean up.
+stream_manager.seal_stream(SCOPE, STREAM);
+stream_manager.delete_stream(SCOPE, STREAM);
+stream_manager.delete_scope(SCOPE);
+```
+
+With a [`pravega-standalone`](https://cncf.pravega.io/docs/latest/deployment/run-local/) running locally, you can see these outputs after running it:
+
+```shell
+$ node --version
+v16.14.0
+$ node index.js
+Event at 0 reads Hello World!
+Event at 20 reads Hello World!
+Event at 40 reads Hello World!
+Event at 60 reads Hello World!
+```
 
 ## Supported APIs
 
-| API details                   | Java | RUST | Python 3.6, 3.7, 3.8, 3.9 | NodeJs  |
-|-------------------------------|------|------|---------------------------|---------|
-| EventWriter                   | X    | X    | X                         | X[3]    |
-| EventReader                   | X    | X    | X[1]                      | X[3]    |
-| ReaderGroup                   | X    | X    | X                         | X[3]    |
-| TxnWriter                     | X    | X    | X                         | -       |
-| Transaction                   | X    | X    | X                         | -       |
-| ByteStreamWriter              | X    | X    | X                         | -       |
-| ByteStreamReader              | X    | X    | X                         | -       |
-| StateSynchronizer             | X    | -    | -                         | -       |
-| TableSynchronizer             | -    | X    | -                         | -       |
-| KeyValueTable                 | X    | X    | -                         | -       |
-| StreamManager#create_scope    | X    | X    | X                         | X[3]    |
-| StreamManager#delete_scope    | X    | X    | X                         | X[3]    |
-| StreamManager#list_scopes     | X    | X    | -                         | X[3]    |
-| StreamManager#create_stream   | X    | X    | X                         | X[3]    |
-| StreamManager#update_stream   | X    | X    | X                         | X[3]    |
-| StreamManager#get_stream_tags | X[2] | X[2] | X[2]                      | X[2][3] |
-| StreamManager#seal_stream     | X    | X    | X                         | X[3]    |
-| StreamManager#delete_stream   | X    | X    | X                         | X[3]    |
-| StreamManager#list_streams    | X    | X    | -                         | X[3]    |
+| API details                   | Java | RUST | Python 3.6, 3.7, 3.8, 3.9 | NodeJs |
+|-------------------------------|------|------|---------------------------|--------|
+| EventWriter                   | X    | X    | X                         | X      |
+| EventReader                   | X    | X    | X[1]                      | X      |
+| ReaderGroup                   | X    | X    | X                         | X      |
+| TxnWriter                     | X    | X    | X                         | -      |
+| Transaction                   | X    | X    | X                         | -      |
+| ByteStreamWriter              | X    | X    | X                         | -      |
+| ByteStreamReader              | X    | X    | X                         | -      |
+| StateSynchronizer             | X    | -    | -                         | -      |
+| TableSynchronizer             | -    | X    | -                         | -      |
+| KeyValueTable                 | X    | X    | -                         | -      |
+| StreamManager#create_scope    | X    | X    | X                         | X      |
+| StreamManager#delete_scope    | X    | X    | X                         | X      |
+| StreamManager#list_scopes     | X    | X    | -                         | X      |
+| StreamManager#create_stream   | X    | X    | X                         | X      |
+| StreamManager#update_stream   | X    | X    | X                         | X      |
+| StreamManager#get_stream_tags | X[2] | X[2] | X[2]                      | X[2]   |
+| StreamManager#seal_stream     | X    | X    | X                         | X      |
+| StreamManager#delete_stream   | X    | X    | X                         | X      |
+| StreamManager#list_streams    | X    | X    | -                         | X      |
 
 1. StreamReader provides an Async Python binding. It requires at least Python 3.6+.
 2. This requires PRAVEGA 0.10.x, enabled as part of PR https://github.com/pravega/pravega-client-rust/pull/281
-3. Local npm and cargo build is required via `npm run build-debug` or `npm run install`.
+
+## Development
+
+To build and test this binding locally, Rust toolchain must be installed and `cargo build` can be executed without any problems in the parent project.
+
+Then you need to install Nodejs related packages via `npm i` in this folder.
+
+### Tests
+
+1. `npm run build-release` to build a debug addon.
+2. `/path/to/pravega/bin/pravega-standalone`
+3. `npm run test`
+
+### Local build and install
+
+1. `npm run release-native` to build a release addon.
+2. `npm run release-js` to build a release dist.
+3. `npm pack` to pack a local npm package.
+4. `npm i pravega-0.4.0.tgz` in your project and use it.
--- nodejs/stream_manager.ts
+++ nodejs/stream_manager.ts
@@ -54,23 +54,50 @@ const {
 import { StreamReaderGroup } from './stream_reader_group.js';
 import { StreamWriter } from './stream_writer.js';
 
+/**
+ * Pravega allows users to store data in Tier 2 as long as there is storage capacity available.
+ * But sometimes, users may not be interested to keep all the historical data related to a Stream.
+ * Instead, there are use-cases in which it may be useful to retain just a fraction of a Stream's data.
+ * For this reason, Streams can be configured with `StreamRetentionPolicy`.
+ */
 export interface StreamRetentionPolicy {}
 
 export const StreamRetentionPolicy = {
+    /**
+     * Every event is retained in the Stream. No deletion.
+     */
     none: (): StreamRetentionPolicy => StreamRetentionPolicyNone(),
+    /**
+     * Set retention based on how many data in the Stream before it is deleted.
+     */
     by_size: (size_in_bytes: number): StreamRetentionPolicy => StreamRetentionPolicyBySize(size_in_bytes),
+    /**
+     * Set retention based on how long the data is kept in the Stream before it is deleted.
+     */
     by_time: (time_in_millis: number): StreamRetentionPolicy => StreamRetentionPolicyByTime(time_in_millis),
 };
 
+/**
+ * A policy that specifies how the number of segments in a stream should scale over time.
+ */
 export interface StreamScalingPolicy {}
 
 export const StreamScalingPolicy = {
+    /**
+     * No scaling, there will only ever be initial_segmentsat any given time.
+     */
     fixed_scaling_policy: (initial_segments: number): StreamScalingPolicy => StreamScalingPolicyFixed(initial_segments),
+    /**
+     * Scale based on the rate in bytes specified in target_rate_kbytes_per_sec.
+     */
     auto_scaling_policy_by_data_rate: (
         target_rate_kbytes_per_sec: number,
         scale_factor: number,
         initial_segments: number
     ): StreamScalingPolicy => StreamScalingPolicyByDataRate(target_rate_kbytes_per_sec, scale_factor, initial_segments),
+    /**
+     * Scale based on the rate in events specified in target_events_per_sec.
+     */
     auto_scaling_policy_by_event_rate: (
         target_events_per_sec: number,
         scale_factor: number,
@@ -102,21 +129,14 @@ export const StreamCut = {
  * const stream_manager = StreamManger('tls://127.0.0.1:9090', false, false, true);
  * ```
  */
-export const StreamManager = (
-    controller_uri: string,
-    auth_enabled: boolean = false,
-    tls_enabled: boolean = false,
-    disable_cert_verification: boolean = true
-) => {
-    const stream_manager = StreamManagerNew(controller_uri, auth_enabled, tls_enabled, disable_cert_verification);
-
+export interface StreamManager {
     /**
      * Create a Pravega scope.
      *
      * @param scope_name The scope name.
      * @returns The scope creation result. `false` indicates that the scope exists before creation.
      */
-    const create_scope = (scope_name: string): boolean => StreamManagerCreateScope.call(stream_manager, scope_name);
+    create_scope: (scope_name: string) => boolean;
 
     /**
      * Delete a Pravega scope.
@@ -124,66 +144,50 @@ export const StreamManager = (
      * @param scope_name The scope name.
      * @returns The scope deletion result. `false` indicates that the scope does not exist before deletion.
      */
-    const delete_scope = (scope_name: string): boolean => StreamManagerDeleteScope.call(stream_manager, scope_name);
+    delete_scope: (scope_name: string) => boolean;
 
     /**
      * List all scopes in Pravega.
      *
      * @returns All scope names.
      */
-    const list_scopes = (): string[] => StreamManagerListScopes.call(stream_manager);
+    list_scopes: () => string[];
 
     /**
      * Create a stream with or without specific policy in Pravega.
      *
      * @param scope_name The scope name.
      * @param stream_name The stream name.
-     * @param scaling_policy The scaling policy.
-     * @param retention_policy The retention policy.
+     * @param retention_policy The retention policy. Default will be StreamRetentionPolicy.none()
+     * @param scaling_policy The scaling policy. Default will be StreamScalingPolicy.fixed_scaling_policy(1)
      * @param tags The stream tags.
      * @returns The stream creation result. `false` indicates that the stream exists before creation.
      */
-    const create_stream = (
+    create_stream: (
         scope_name: string,
         stream_name: string,
-        scaling_policy: StreamScalingPolicy = StreamRetentionPolicy.none(),
-        retention_policy: StreamRetentionPolicy = StreamScalingPolicy.fixed_scaling_policy(1),
-        tags: string[] = []
-    ): boolean =>
-        StreamManagerCreateStreamWithPolicy.call(
-            stream_manager,
-            scope_name,
-            stream_name,
-            scaling_policy,
-            retention_policy,
-            tags
-        );
+        retention_policy?: StreamRetentionPolicy,
+        scaling_policy?: StreamScalingPolicy,
+        tags?: string[]
+    ) => boolean;
 
     /**
      * Update a Pravega stream with new policies and tags.
      *
      * @param scope_name The scope name.
      * @param stream_name The stream name.
-     * @param scaling_policy The scaling policy.
-     * @param retention_policy The retention policy.
+     * @param retention_policy The retention policy. Default will be StreamRetentionPolicy.none()
+     * @param scaling_policy The scaling policy. Default will be StreamScalingPolicy.fixed_scaling_policy(1)
      * @param tags The stream tags.
      * @returns The stream update result.
      */
-    const update_stream = (
+    update_stream: (
         scope_name: string,
         stream_name: string,
-        scaling_policy: StreamScalingPolicy = StreamRetentionPolicy.none(),
-        retention_policy: StreamRetentionPolicy = StreamScalingPolicy.fixed_scaling_policy(1),
-        tags: string[] = []
-    ): boolean =>
-        StreamManagerUpdateStreamWithPolicy.call(
-            stream_manager,
-            scope_name,
-            stream_name,
-            scaling_policy,
-            retention_policy,
-            tags
-        );
+        retention_policy?: StreamRetentionPolicy,
+        scaling_policy?: StreamScalingPolicy,
+        tags?: string[]
+    ) => boolean;
 
     /**
      * Get tags of a Pravega stream.
@@ -192,8 +196,7 @@ export const StreamManager = (
      * @param stream_name The stream name.
      * @returns The stream tags.
      */
-    const get_stream_tags = (scope_name: string, stream_name: string): string[] =>
-        StreamManagerGetStreamTags.call(stream_manager, scope_name, stream_name);
+    get_stream_tags: (scope_name: string, stream_name: string) => string[];
 
     /**
      * Seal a Pravega stream. SEAL BEFORE DELETE!
@@ -202,8 +205,7 @@ export const StreamManager = (
      * @param stream_name The stream name.
      * @returns The seal result.
      */
-    const seal_stream = (scope_name: string, stream_name: string): boolean =>
-        StreamManagerSealStream.call(stream_manager, scope_name, stream_name);
+    seal_stream: (scope_name: string, stream_name: string) => boolean;
 
     /**
      * Deleta a Pravega stream. SEAL BEFORE DELETE!
@@ -212,16 +214,15 @@ export const StreamManager = (
      * @param stream_name The stream name.
      * @returns The deletion result.
      */
-    const delete_stream = (scope_name: string, stream_name: string): boolean =>
-        StreamManagerDeleteStream.call(stream_manager, scope_name, stream_name);
+    delete_stream: (scope_name: string, stream_name: string) => boolean;
 
     /**
-     * List all scopes in Pravega.
+     * List all streams in the specified Pravega scope.
      *
      * @param scope_name The scope name.
      * @returns All stream names in this scope.
      */
-    const list_streams = (scope_name: string): string[] => StreamManagerListStreams.call(stream_manager, scope_name);
+    list_streams: (scope_name: string) => string[];
 
     /**
      * Create a ReaderGroup for a given Stream.
@@ -233,15 +234,12 @@ export const StreamManager = (
      * @returns A StreamReaderGroup.
      * @todo An optional element cannot follow a rest element. `...args: [...stream: string[], stream_cut?: StreamCut]`
      */
-    const create_reader_group = (
+    create_reader_group: (
         stream_cut: StreamCut,
         reader_group_name: string,
         scope_name: string,
         ...streams: string[]
-    ): StreamReaderGroup =>
-        StreamReaderGroup(
-            StreamManagerCreateReaderGroup.call(stream_manager, reader_group_name, scope_name, streams, stream_cut)
-        );
+    ) => StreamReaderGroup;
 
     /**
      * Create a Writer for a given Stream.
@@ -256,14 +254,76 @@ export const StreamManager = (
      *  when `write_event(...)` and `write_event_bytes(...)` are returned.
      * @returns A StreamWriter.
      */
-    const create_writer = (scope_name: string, stream_name: string, max_inflight_events: number = 0): StreamWriter =>
-        StreamWriter(StreamManagerCreateWriter.call(stream_manager, scope_name, stream_name, max_inflight_events));
+    create_writer: (scope_name: string, stream_name: string, max_inflight_events?: number) => StreamWriter;
 
     /**
-     * A detailed view of a StreamManager.
+     * A detailed view of the StreamManager.
      *
      * @returns String representation of the StreamManager.
      */
+    toString: () => string;
+}
+
+export const StreamManager = (
+    controller_uri: string,
+    auth_enabled: boolean = false,
+    tls_enabled: boolean = false,
+    disable_cert_verification: boolean = true
+): StreamManager => {
+    // The internal rust StreamManager object. Should not be accessed directly.
+    const stream_manager = StreamManagerNew(controller_uri, auth_enabled, tls_enabled, disable_cert_verification);
+
+    const create_scope = (scope_name: string): boolean => StreamManagerCreateScope.call(stream_manager, scope_name);
+    const delete_scope = (scope_name: string): boolean => StreamManagerDeleteScope.call(stream_manager, scope_name);
+    const list_scopes = (): string[] => StreamManagerListScopes.call(stream_manager);
+    const create_stream = (
+        scope_name: string,
+        stream_name: string,
+        retention_policy: StreamRetentionPolicy = StreamRetentionPolicy.none(),
+        scaling_policy: StreamScalingPolicy = StreamScalingPolicy.fixed_scaling_policy(1),
+        tags: string[] = []
+    ): boolean =>
+        StreamManagerCreateStreamWithPolicy.call(
+            stream_manager,
+            scope_name,
+            stream_name,
+            retention_policy,
+            scaling_policy,
+            tags
+        );
+    const update_stream = (
+        scope_name: string,
+        stream_name: string,
+        retention_policy: StreamRetentionPolicy = StreamRetentionPolicy.none(),
+        scaling_policy: StreamScalingPolicy = StreamScalingPolicy.fixed_scaling_policy(1),
+        tags: string[] = []
+    ): boolean =>
+        StreamManagerUpdateStreamWithPolicy.call(
+            stream_manager,
+            scope_name,
+            stream_name,
+            retention_policy,
+            scaling_policy,
+            tags
+        );
+    const get_stream_tags = (scope_name: string, stream_name: string): string[] =>
+        StreamManagerGetStreamTags.call(stream_manager, scope_name, stream_name);
+    const seal_stream = (scope_name: string, stream_name: string): boolean =>
+        StreamManagerSealStream.call(stream_manager, scope_name, stream_name);
+    const delete_stream = (scope_name: string, stream_name: string): boolean =>
+        StreamManagerDeleteStream.call(stream_manager, scope_name, stream_name);
+    const list_streams = (scope_name: string): string[] => StreamManagerListStreams.call(stream_manager, scope_name);
+    const create_reader_group = (
+        stream_cut: StreamCut,
+        reader_group_name: string,
+        scope_name: string,
+        ...streams: string[]
+    ): StreamReaderGroup =>
+        StreamReaderGroup(
+            StreamManagerCreateReaderGroup.call(stream_manager, reader_group_name, scope_name, streams, stream_cut)
+        );
+    const create_writer = (scope_name: string, stream_name: string, max_inflight_events: number = 0): StreamWriter =>
+        StreamWriter(StreamManagerCreateWriter.call(stream_manager, scope_name, stream_name, max_inflight_events));
     const toString = (): string => StreamManagerToString.call(stream_manager);
 
     return {
