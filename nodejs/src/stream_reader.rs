64a58484e023e9534f46ec1c8d51a47641881e65
thekingofcity <3353040+thekingofcity@users.noreply.github.com>
2022-04-25 10:29:04 +0800

    fix typo
    Signed-off-by: thekingofcity <3353040+thekingofcity@users.noreply.github.com>

--- nodejs/src/stream_reader.rs
+++ nodejs/src/stream_reader.rs
@@ -133,7 +133,7 @@ impl StreamReader {
 impl StreamReader {
     ///
     /// Return a Slice in an asynchronous call.
-    /// The actural returned type from await will be `Promise<Slice>` aka `JsResult<JsBoxed<RefCell<Slice>>>`.
+    /// The actual returned type from await will be `Promise<Slice>` aka `JsResult<JsBoxed<RefCell<Slice>>>`.
     ///
     /// See the tokio-fetch example for more details on how to return a Promise and await.
     /// https://github.com/neon-bindings/examples/tree/2dbbef55f483635d0118c20c9902bf4c6faa1ecc/examples/tokio-fetch
