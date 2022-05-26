7264e8728a3b82841232dfe42c69e90926c33e1d
Tom Kaitchuck <tkaitchuck@users.noreply.github.com>
2022-05-25 20:56:11 -0700

    Remove stray comma

--- config/src/connection_type.rs
+++ config/src/connection_type.rs
@@ -32,6 +32,6 @@ impl Default for ConnectionType {
 
 impl fmt::Display for ConnectionType {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?}", self),
+        write!(f, "{:?}", self)
     }
 }
