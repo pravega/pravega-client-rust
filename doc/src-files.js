d789dc3b3710b5aab21eb10299fa46ba46636ba3
tkaitchuck <tkaitchuck@users.noreply.github.com>
2024-02-23 17:34:57 +0000

    deploy: 4778dc5880148e96751fa3d52cf6cc5809ab96b9

--- doc/src-files.js
+++ doc/src-files.js
@@ -1,4 +1,4 @@
-var srcIndex = JSON.parse('{\
-"pravega_client":["",[["byte",[],["mod.rs","reader.rs","writer.rs"]],["event",[],["mod.rs","reader.rs","reader_group.rs","reader_group_state.rs","transactional_writer.rs","writer.rs"]],["index",[],["mod.rs","reader.rs","writer.rs"]],["segment",[],["event.rs","large_writer.rs","metadata.rs","mod.rs","raw_client.rs","reactor.rs","reader.rs","selector.rs","writer.rs"]],["sync",[],["mod.rs","synchronizer.rs","table.rs"]],["util",[],["meta.rs","metric.rs","mod.rs","oneshot_holder.rs"]]],["client_factory.rs","error.rs","lib.rs","test_utils.rs"]]\
-}');
+var srcIndex = new Map(JSON.parse('[\
+["pravega_client",["",[["byte",[],["mod.rs","reader.rs","writer.rs"]],["event",[],["mod.rs","reader.rs","reader_group.rs","reader_group_state.rs","transactional_writer.rs","writer.rs"]],["index",[],["mod.rs","reader.rs","writer.rs"]],["segment",[],["event.rs","large_writer.rs","metadata.rs","mod.rs","raw_client.rs","reactor.rs","reader.rs","selector.rs","writer.rs"]],["sync",[],["mod.rs","synchronizer.rs","table.rs"]],["util",[],["meta.rs","metric.rs","mod.rs","oneshot_holder.rs"]]],["client_factory.rs","error.rs","lib.rs","test_utils.rs"]]]\
+]'));
 createSrcSidebar();
